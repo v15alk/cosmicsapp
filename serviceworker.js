c7c4798b5ffb0626b856370fc2900cec3fe80213
v15alk <v.kamaleswaran.23@unimail.winchester.ac.uk>
2025-02-24 11:02:24 +0000

    add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'paw-landing-page-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js'
+];
+
+// Install service worker and cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve cached files when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
